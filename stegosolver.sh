2921b732f844f0c5a14d861c973e45f3f22b8bfe
fieldraccoon <66012425+fieldraccoon@users.noreply.github.com>
2020-10-27 12:43:10 +0000

    Create stegosolver.sh

---
+++ stegosolver.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+mkdir results
+
+
+base_file=$1
+
+echo -e "Your file you are using is $base_file"
+
+
+file_ext=$(sed -e 's/.*\.//' <<< "$base_file")
+
+echo "file extension is $file_ext"
+
+
+if [ ! -z "$base_file" ]; then
+	strings $base_file > results/strings.txt
+	binwalk $base_file > results/binwalk.txt
+       	cd results
+	binwalk -e $base_file
+	cd ..
+	zsteg $base_file > results/zsteg.txt
+
+	
+
+
+else
+	echo "Sorry your file is empty. Please use a valid file"
+fi
+
+if [ "$file_ext" == "txt" ]
+then
+	echo "this is a txt file"
+	stegsnow -C $base_file > results/stegsnow.txt
+fi
+
+
+if [ "$file_ext" == "jpg" ] || [ "$file_ext" == "jpg" ]; then
+	echo "this is a jpeg file"
+	exiftool $base_file > results/exiftool.txt
+	steghide extract -sf $base_file > results/steghide.txt
+fi
+
+if [ "$file_ext" == "png" ]; then
+	exiftool $base_file > results/exiftool.txt
+fi
